2b734497df8a7c325a836546a0d2deb150555515
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2022-02-20 22:04:44 -0300

    experiments with fltk-rs

--- desktop_client/src/main.rs
+++ desktop_client/src/main.rs
@@ -1,9 +1,17 @@
-use fltk::{app, prelude::*, window::Window};
+use fltk::{app::App, app::Scheme::Gtk, prelude::*, window::Window, frame::Frame, button::Button};
 
 fn main() {
-    let app = app::App::default();
+    let mut counter: u8 = 0;
+    let app = App::default().with_scheme(Gtk);
     let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let mut frame = Frame::new(0, 0, 400, 200, "");
+    let mut but = Button::new(160, 210, 80, 40, "Click me!");
     wind.end();
     wind.show();
+    but.set_callback(move |_| {
+        let label = format!("Hello world: {}", counter);
+        frame.set_label(label.as_str());
+        counter += 1;
+    });
     app.run().unwrap();
 }
